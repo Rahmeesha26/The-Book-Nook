e73bff3f2cf9baf006dec3be4f3c4c48af1209ca
rahmeesha26 <rahmeeshar91@gmail.com>
2023-06-02 15:54:20 -0400

    typed up some js to see the api

--- main.js
+++ main.js
@@ -4,6 +4,35 @@ $searchForm.on("find-it", event => {
           event.preventDefault();
 
           const formData = new FormData(event.target);
-          const who = formData.get("who")
-          console.log("who")
-})
+
+          const who = formData.get("who").toLocaleLowerCase();
+
+          const url = `https://openlibrary.org/dev/docs/api/search.json?q=${who}`
+
+
+          console.log(url)
+
+          const $photo = $('.parent')
+          
+          fetch(url)
+          .then(response => {
+                    return response.json();
+          })
+          .then(data => {
+                    $photo.html(`<img src=${data.title.front_default} alt=${data.author} alt=${data.genre}>`);
+                    $info-box.html(
+                             ` <div>
+                                        <b>title: </b> ${data.title}
+                              </div>
+                              <div>
+                              <div>
+                                        <b>name: </b> ${data.name}
+                              </div>
+                                        <b>date: </b> ${data.date}
+                              </div>
+                              <div>
+                                        <b>isbn: </b> ${data.isbn}
+                              </div>`
+                    )
+          })
+});
